2f179f438d0f3a01c832d46c2da3dede97a7b841
Saeed_5077 <saeedanwaransari573@gmail.com>
2023-05-27 21:45:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function openMenu()
+{
+    document.querySelector("div#mylinks ").style.display= "block";
+    document.querySelector("a#cutbtn").style.display="block";
+
+}
+function CloseMenu()
+{
+    document.getElementById("menubar").style.left= "-100vw";
+}
